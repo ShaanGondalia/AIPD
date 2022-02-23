44a294244f958229b18f5380000637105ef39b73
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-23 10:45:24 -0500

    Added agents

---
+++ agent/cooperate_agent.py
@@ -0,0 +1,9 @@
+from base_agent import BaseAgent
+
+class CooperateAgent(BaseAgent):
+
+  def play(self):
+    return 0
+
+  def opt(self):
+    return 1
---
+++ agent/grudge_agent.py
@@ -0,0 +1,12 @@
+from base_agent import BaseAgent
+
+class GrudgeAgent(BaseAgent):
+
+  def update(self, val):
+    if val == 1:
+      self.val = 1
+  
+  def opt(self):
+    if self.val == 0:
+      return 0
+    return 1
