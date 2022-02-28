834fa33ed221695b71c25cd0158ecbcf0d6b8c8e
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-02-28 10:29:55 -0500

    Added agents

---
+++ agent/copy_agent.py
@@ -0,0 +1,6 @@
+from base_agent import BaseAgent
+
+class CopyAgent(BaseAgent):
+
+  def opt(self):
+    return 0
---
+++ agent/defect_agent.py
@@ -0,0 +1,9 @@
+from base_agent import BaseAgent
+
+class DefectAgent(BaseAgent):
+
+  def play(self):
+    return 1
+  
+  def opt(self):
+    return 1
