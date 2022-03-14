9eb793adf810fb36c2460bd57ef9dcdca7c1cff0
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-03-14 15:38:52 -0400

    Fixed typos

--- lstm/main.py
+++ lstm/main.py
@@ -1,13 +1,12 @@
 from lstm import LSTM
 from dataset import PreTrainDataset
 import hyper_parameters as hp
-import numpy as np
 
 def main():
 	dataset = PreTrainDataset()
 	model = LSTM(hp.IN, hp.HIDDEN, hp.OUT, hp.ID, hp.LAYERS)
 	model.pretrain(dataset)
-	mode.learn()
+	model.learn()
 
 if __name__ == "__main__":
 	main()
