4ba8213a65e0e69f619c7752922a5f94ba155285
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-03-28 16:29:22 -0400

    Updated game to use new agent API

--- game.py
+++ game.py
@@ -9,22 +9,25 @@ class Game():
 		self.train = train
 		self.load = load
 		self.save = save
+		self.agents = ag.Agents() # The agents to play against in the tournament
 		if self.train:
-			self.lstm.pretrain()
+			self.lstm.train()
+			if not self.load:
+				self.lstm.pretrain(self.agents)
 		else:
 			self.lstm.eval()
 		if self.load:
 			self.lstm.load(self.load)
 
 	def play(self):
-		agents = ag.Agents() # The agents to play against in the tournament
-
+		self.lstm.eval()
 		for epoch in range(EPOCHS):
 			print("EPOCH %d" % epoch)
 			errors = 0
 			for i in tqdm(range(GAMES)):
-				agent = agents.get_random_agent()
+				agent = self.agents.get_random_agent()
 				errors += self._play_one_game(agent)
+				self.agents.reset_all_agents()
 
 			frac = (GAMES-errors)/GAMES
 			print("Prediction Accuracy: %.2f" % frac)
@@ -33,7 +36,7 @@ class Game():
 
 	def _play_one_game(self, agent):
 		"""Plays a single game against an agent, comprised of ROUNDS iterations"""
-		prev_agent_choice = agent.previous()
+		prev_agent_choice = agent.play()
 		input = self.lstm.build_input_vector(prev_agent_choice)
 		id = self.lstm.build_id_vector(agent)
 		# Play ROUNDS iterations of the prisoners dilemma against the same agent
