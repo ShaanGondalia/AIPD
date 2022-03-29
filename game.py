2d0a226af0e53f1e101a76bd37297a7e5d4673a4
Shaan Gondalia <shaan.gondalia@duke.edu>
2022-03-29 16:24:53 -0400

    Fixed bugs with q table integration

--- game.py
+++ game.py
@@ -16,7 +16,7 @@ class Game():
         self.agents = ag.Agents() # The agents to play against in the tournament
         self.q_agents = {}
         for agent in self.agents.agents:
-            self.q_agents[agent.id] = qag.QAgent(lr = qhp.LR, 
+            self.q_agents[agent.id()] = qag.QAgent(lr = qhp.LR, 
                 discount = qhp.DISCOUNT, epsilon=qhp.EPSILON_TRAIN, 
                 decay_rate=qhp.DECAY_RATE, min_e=qhp.MIN_EPSILON, memory=qhp.MEMORY)
 
@@ -26,7 +26,7 @@ class Game():
         self.lstm.pretrain(self.agents)
         print("Training QTables")
         for agent in self.agents.agents:
-            ql.train(self.q_agents[agent.id], agent)
+            ql.train(self.q_agents[agent.id()], agent)
 
     def save(self, fname):
         print(f"Saving Models to file: {fname}")
@@ -57,20 +57,22 @@ class Game():
     def _play_one_game(self, agent):
         """Plays a single game against an agent, comprised of ROUNDS iterations"""
         prev_agent_choice = agent.play()
-        prev_moves = np.array([[], []]).T
+        prev_agent_moves = []
+        prev_nn_moves = []
         input = self.lstm.build_input_vector(prev_agent_choice)
         id = self.lstm.build_id_vector(agent)
         # Play ROUNDS iterations of the prisoners dilemma against the same agent
         for _ in range(ROUNDS):
+            prev_moves = np.array([prev_nn_moves, prev_agent_moves]).T
             pred_id, id_logits = self.lstm.predict_id(input, agent)
             probs = nnf.softmax(id_logits, dim=1).detach().cpu().numpy()
             agent_action = int(agent.play())
             # TODO: Implement Linear combination of results here
             nn_action = self.q_agents[pred_id].pick_action(prev_moves, False)
-            prev_moves = np.hstack(prev_moves, np.array([nn_action, agent_action]).T)
-
             input = self.lstm.rebuild_input(nn_action, agent_action, input[0])
             agent.update(nn_action)
+            prev_agent_moves.append(agent_action)
+            prev_nn_moves.append(nn_action)
         # self.lstm.learn(id_logits, id)
 
         return 0 if pred_id == agent.id() else 1
