87b81033d0bcd286ec7cc38ffa77a14aee344b19
SW386 <sw386@duke.edu>
2022-04-25 07:39:53 -0400

    feat: completed website backend

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
-from models import MiniAgent
+from models import AIAgent, Tournament
+import base64
 
 app = Flask(__name__)
 CORS(app)
-agent = MiniAgent("default")
+agent = AIAgent("default")
 
 def validate_moves(moves):
     valid_moves = set([0, 1])
@@ -38,3 +39,27 @@ def get_agent_move():
 
     return {'agent_decision' : agent_decision}, 200
 
+@app.route('/tournament', methods=['POST'])
+def get_tournament_visual():
+
+    generations = request.json.get('generations')
+    interactions = request.json.get('interactions')
+    rounds = request.json.get('rounds')
+    reproduction_rate = request.json.get('reproduction_rate')
+    config = request.json.get('config')
+
+    if 'agents' not in config:
+        return {'message': 'config is not properly formatted'}, 400
+    if type(generations) != int:
+        return {'message': 'generations must be a number'}, 400
+    if type(interactions) != int:
+        return {'message': 'interactions must be a number'}, 400
+    if type(rounds) != int:
+        return {'message': 'rounds must be a number'}, 400
+    if type(reproduction_rate) != float or reproduction_rate > 1 or reproduction_rate <= 0:
+        return {'message': 'reproduction rate must be decimal between 0 and 1'}, 400
+
+    tournament = Tournament(generations, interactions, rounds, reproduction_rate, config)
+    buffer = tournament.tournament()
+    gif_base64 = base64.b64encode(buffer.getbuffer()).decode("ascii")
+    return {'gif': gif_base64}, 200
--- backend/lstm/lstm.py
+++ backend/lstm/lstm.py
@@ -27,3 +27,22 @@ class LSTM(nn.Module):
         state_dict = torch.load(f"saved/{fname}.pth", map_location=torch.device('cpu'))
         self.load_state_dict(state_dict)
         self.to(self.device)
+
+    def build_input_vector(self, prev_agent_choice):
+        """Creates NN input vector"""
+        input = [0, 0]
+        input[1] = prev_agent_choice
+        return torch.Tensor(input).to(self.device).unsqueeze(0).unsqueeze(0)
+
+    def build_id_vector(self, agent):
+        """Find id of agent (another input to NN for regularization)"""
+        id = [agent.id()]
+        id = torch.Tensor(id).to(self.device)
+        return id.to(torch.int64)
+
+    def rebuild_input(self, nn_action, opp_action, prev_input):
+        curr_input = [0, 0]
+        curr_input[0] = nn_action
+        curr_input[1] = opp_action
+        curr_input = torch.Tensor(curr_input).to(self.device).unsqueeze(0)
+        return torch.cat([prev_input, curr_input]).unsqueeze(0)
